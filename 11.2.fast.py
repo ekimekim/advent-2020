05d442112dd94d7d9dbb1c3558d4342c4e91c4ae
Mike Lang <mikelang3000@gmail.com>
2020-12-12 07:36:31 +1100

    improve 11.2 again by pre-calculating neighbor positions (only raycast once)

--- 11.2.fast.py
+++ 11.2.fast.py
@@ -10,21 +10,27 @@ ROW_LEN = len(seats[0])
 COL_LEN = len(seats)
 seats = sum(seats, [])
 
+neighbors = [None for _ in range(len(seats))]
 def cast(seats, x, y, dx, dy):
 	while True:
 		x += dx
 		y += dy
 		if x < 0 or y < 0 or y >= COL_LEN or x >= ROW_LEN:
-			return False
+			return None
 		c = seats[y * ROW_LEN + x]
 		if c != '.':
-			return c == '#'
+			return x, y
+for y in range(COL_LEN):
+	for x in range(ROW_LEN):
+		found = []
+		for dx, dy in itertools.product((-1, 0, 1), (-1, 0, 1)):
+			if dx == 0 and dy == 0:
+				continue
+			neighbor = cast(seats, x, y, dx, dy)
+			if neighbor:
+				found.append(neighbor)
+		neighbors[y * ROW_LEN + x] = found
 
-DELTAS = [
-	(dx, dy)
-	for dx, dy in itertools.product((-1, 0, 1), (-1, 0, 1))
-	if dx != 0 or dy != 0
-]
 
 def step(seats):
 	new_seats = seats[:]
@@ -33,15 +39,15 @@ def step(seats):
 			if seats[y * ROW_LEN + x] == '.':
 				continue
 			elif seats[y * ROW_LEN + x] == 'L':
-				for dx, dy in DELTAS: 
-					if cast(seats, x, y, dx, dy):
+				for nx, ny in neighbors[y * ROW_LEN + x]: 
+					if seats[ny * ROW_LEN + nx] == '#':
 						break
 				else:
 					new_seats[y * ROW_LEN + x] = '#'
 			else:
 				count = 0
-				for dx, dy in DELTAS:
-					if cast(seats, x, y, dx, dy):
+				for nx, ny in neighbors[y * ROW_LEN + x]: 
+					if seats[ny * ROW_LEN + nx] == '#':
 						count += 1
 						if count == 5:
 							new_seats[y * ROW_LEN + x] = 'L'
@@ -49,9 +55,13 @@ def step(seats):
 
 	return new_seats
 
+def p(seats):
+	for y in range(COL_LEN):
+		print ''.join(seats[y * COL_LEN : (y+1) * COL_LEN])
 
 while True:
 	new_seats = step(seats)
+	# p(new_seats)
 	if seats == new_seats:
 		break
 	seats = new_seats
