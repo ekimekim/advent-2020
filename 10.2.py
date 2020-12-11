f0c873c81970fde1f8b06ca8e718786bca130087
Mike Lang <mikelang3000@gmail.com>
2020-12-11 11:02:01 +1100

    10.2: slightly cleaner

--- 10.2.py
+++ 10.2.py
@@ -1,5 +1,5 @@
 import sys
-from collections import Counter
+from itertools import count
 
 joltages = map(int, sys.stdin)
 joltages = [0] + sorted(joltages) + [max(joltages) + 3]
@@ -16,10 +16,10 @@ def combinations(index):
 		return 1
 	current = joltages[index]
 	found = 0
-	for i, next in enumerate(joltages[index+1:]):
-		if next - current > 3:
+	for next_index in range(index+1, len(joltages)):
+		if joltages[next_index] - current > 3:
 			break
-		found += memo(index + i + 1)
+		found += memo(next_index)
 	return found
 
 print combinations(0)
