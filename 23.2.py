3f2da80c088a4174357c2b6667b568e253e651a3
Mike Lang <mikelang3000@gmail.com>
2020-12-24 08:16:09 +1100

    23 part 2

--- 23.2.py
+++ 23.2.py
@@ -1,34 +1,47 @@
-import sys
+
 import itertools
+import sys
 
-cups = map(int, sys.stdin.read().strip())
-max_cup = max(cups)
+cups = {}
+
+class Cup(object):
+	__slots__ = ['label', 'next']
+	def __init__(self, label):
+		self.label = label
+		cups[label] = self
+		self.next = None
+
+def append(cup, label):
+	new_cup = Cup(label)
+	cup.next = new_cup
+	return new_cup
+
+inputs = map(int, sys.stdin.read().strip())
+current = Cup(inputs[0])
+tail = current
 max_label = 1000001
-cups += range(max_cup + 1, max_label)
 
-def step(index):
-	picked_up = cups[index+1:index+4]
-#	print "picked up", picked_up, "from", index+1, "to", index+4
-	for label in itertools.count(cups[index] - 1, -1):
+for input in inputs[1:]:
+	tail = append(tail, input)
+for i in range(max(inputs) + 1, 1000001):
+	tail = append(tail, i)
+tail.next = current
+
+for _ in range(10000000):
+	picked_up = current.next
+	picked_up_labels = (
+		picked_up.label,
+		picked_up.next.label,
+		picked_up.next.next.label,
+	)
+	current.next = picked_up.next.next.next
+	for label in itertools.count(current.label - 1, -1):
 		label = label % max_label
-		if label != 0 and label not in picked_up:
-			dest_cup = label
+		if label != 0 and label not in picked_up_labels:
+			dest_cup = cups[label]
 			break
-	dest_index = cups.index(dest_cup)
-#	print "count down from", cups[index], "to", dest_cup, "at", dest_index
+	picked_up.next.next.next = dest_cup.next
+	dest_cup.next = picked_up
+	current = current.next
 
-	to_move = (dest_index - index - 3) % len(cups)
-#	print "moving", to_move, "down"
-	for i in range(index + 1, index + 1 + to_move):
-#		print "cups[{}] = {} -> {}".format(i % len(cups), cups[i % len(cups)], cups[(i + 3) % len(cups)])
-		cups[i % len(cups)] = cups[(i + 3) % len(cups)]
-	for i in range(3):
-#		print "cups[{}] = {} -> {}".format((index + 1 + to_move + i) % len(cups), cups[(index + 1 + to_move + i) % len(cups)], picked_up[i])
-		cups[(index + 1 + to_move + i) % len(cups)] = picked_up[i]
-
-for i in range(10000000):
-	print "{:.2f}%".format(i / 100000.)
-	step(i % len(cups))
-
-one_index = cups.index(1)
-print cups[(one_index + 1) % len(cups)] * cups[(one_index + 2) % len(cups)]
+print cups[1].next.label * cups[1].next.next.label
