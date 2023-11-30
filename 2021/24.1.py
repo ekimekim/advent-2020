037da3e3834b4b36f60b631ec8b121419c4633a2
Mike Lang <mikelang3000@gmail.com>
2023-11-30 14:57:37 +1100

    wip:

--- 2021/24.1.py
+++ 2021/24.1.py
@@ -2,52 +2,74 @@ import sys
 
 instrs = sys.stdin.read().strip().split("\n")
 
-# value is (op, value, value) or ('literal', int) or ('digit', digit number)
+# value is a list of parts
+# each part is (scalar, bases)
+# bases is a list of ('digit', index) or (op, value, value)
+# part value = scalar * product(bases)
+# total value = sum(parts)
 
-regs = {k: ('literal', 0) for k in 'wxyz'}
+def scalar(value):
+	if value == 0:
+		return ()
+	# scalar has no bases
+	return ((value, ()),)
+
+def digit(index):
+	# scale of 1, single base
+	return ((1, (('digit', index),)),)
+
+def wrap(op, a, b):
+	# scale of 1, base = the wrapped op
+	return ((1, ((op, a, b),)),)
+
+def simplify(parts):
+	# find any common bases and combine them
+	result = []
+	for scalar, bases in parts:
+		found = False
+		for i, (s, b) in enumerate(result):
+			if bases == b:
+				result[i] = (scalar + s, bases)
+				found = True
+		if not found:
+			result.append((scalar, bases))
+	result = [(s, b) for s, b in result if s != 0]
+	return tuple(sorted(result))
+
+regs = {k: () for k in 'wxyz'}
 def parse(s):
 	if s in 'wxyz':
 		return regs[s]
 	else:
-		return ('literal', int(s))
+		return scalar(int(s))
 
-digit = 0
+index = 0
 for instr in instrs:
 	op, rest = instr.split(" ", 1)
 	if op == "inp":
-		regs[rest] = ('digit', digit)
-		digit += 1
+		regs[rest] = digit(index)
+		index += 1
 		continue
 	a, b = rest.split()
 	av = parse(a)
 	bv = parse(b)
-	regs[a] = (op, av, bv)
 
-# condition is a list of 14 digits, each digit is a non-empty frozenset of allowed numbers
-# if a set would be empty, the whole thing is None (contradiction)
-TOP = (frozenset(range(1, 10)),) * 14
+	if op == 'add':
+		# just combine the parts and simplify
+		new = av + bv
+	elif op == 'mul':
+		# multiply every base from A with every base from B,
+		# ie. (ax + by)(cw + dz) = acxw + adxz + bcyw + bdyz
+		result = []
+		for a_s, a_b in av:
+			for b_s, b_b in bv:
+				result.append((a_s * b_s, tuple(sorted(a_b + b_b))))
+		new = tuple(result)
+	else:
+		# we can't do anything sensible here
+		new = wrap(op, av, bv)
 
-def merge(a, b):
-	if None in (a, b):
-		return None
-	ret = tuple(
-		ad & bd
-		for ad, bd in zip(a, b)
-	)
-	if frozenset() in ret:
-		return None
-	return ret
+	print op, av, bv, "->", new
+	regs[a] = simplify(new)
 
-
-def solve(value, targets):
-	if value[0] == 'literal':
-		# literals either allow any digit, or indicate a contradiction
-		if value[1] in targets:
-			return TOP
-		else:
-			return None
-	if value[0] == 'digit':
-		# digit values give no information
-
-z = regs['z']
-solve(z, [0])
+print regs['z']
