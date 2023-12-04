1a92995a1338a2a70c1de938209424577c347021
Mike Lang <mikelang3000@gmail.com>
2023-12-04 16:09:59 +1100

    day 4

--- 2023/4.py
+++ 2023/4.py
@@ -2,13 +2,24 @@ import sys
 
 lines = sys.stdin.read().strip().split("\n")
 
-t = 0
+results = {}
 for line in lines:
 	card, numbers = line.split(": ")
+	_, card = card.split()
+	card = int(card)
 	wins, mine = numbers.split(" | ")
 	wins = set(map(int, wins.split()))
 	mine = set(map(int, mine.split()))
-	got = len(mine & wins)
-	if got:
-		t += 2**(got - 1)
-print t
+	results[card] = len(mine & wins)
+
+print sum([2**(wins - 1) for wins in results.values() if wins > 0])
+
+won = 0
+cards = list(results.items())
+while cards:
+	id, wins = cards.pop()
+	won += 1
+	for n in range(wins):
+		cards.append((id + n + 1, results[id + n + 1]))
+
+print won
