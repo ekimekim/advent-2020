69aa9800db1d07a535d668aa69f798b55e41a13d
Mike Lang <mikelang3000@gmail.com>
2024-12-07 03:30:16 +1100

    2019 day 1 part 1

--- 2019-rust/src/main.rs
+++ 2019-rust/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), String> {
+	println!("{}", day1()?);
+	Ok(())
+}
+
+fn day1() -> Result<usize, String> {
+	std::io::stdin().lines()
+		.enumerate()
+		.map(|(n, result)| match result {
+			Ok(line) => match line.parse::<usize>() {
+				Ok(fuel) => Ok(fuel / 3 - 2),
+				Err(e) => Err(format!("Line {n} parse error: {e}")),
+			},
+			Err(e) => Err(format!("Line {n} IO error: {e}")),
+		})
+		.sum()
 }
