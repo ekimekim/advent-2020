acab719896f63d531ffd925a0f7fee70900dd1f3
Mike Lang <mikelang3000@gmail.com>
2024-12-07 03:48:00 +1100

    day 1 part 2

--- 2019-rust/src/main.rs
+++ 2019-rust/src/main.rs
@@ -8,7 +8,11 @@ fn day1() -> Result<usize, String> {
 		.enumerate()
 		.map(|(n, result)| match result {
 			Ok(line) => match line.parse::<usize>() {
-				Ok(fuel) => Ok(fuel / 3 - 2),
+				Ok(module_mass) => Ok(
+					std::iter::successors(Some(module_mass),
+						|mass| (mass / 3).checked_sub(2)
+					).skip(1).sum::<usize>()
+				),
 				Err(e) => Err(format!("Line {n} parse error: {e}")),
 			},
 			Err(e) => Err(format!("Line {n} IO error: {e}")),
